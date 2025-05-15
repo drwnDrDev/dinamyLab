5ba9e7189992f42bb4fc8801f42ea5d659e18946
Foto Aleph <fotoaleph@gmail.com>
2025-05-14 21:07:55 -0500

    Migraciones y  Modelos, falta agragar las propiedades y relaciones

---
+++ app/Http/Controllers/TributoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tributo;
+use Illuminate\Http\Request;
+
+class TributoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tributo $tributo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tributo $tributo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tributo $tributo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tributo $tributo)
+    {
+        //
+    }
+}
---
+++ app/Models/Prestador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prestador extends Model
+{
+    //
+
+    protected $table = 'prestadores';
+}
