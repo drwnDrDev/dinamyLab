91ecbbfe1ab991156a82e786cfb96e514488bfb5
Foto Aleph <fotoaleph@gmail.com>
2025-06-03 11:10:37 -0500

    refactor: mejorar estilos y estructura en la vista de creación de órdenes

--- resources/views/ordenes/create.blade.php
+++ resources/views/ordenes/create.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-text leading-tight">
             {{ __('Crear Orden') }}
         </h2>
     </x-slot>
 
     <x-canva>
-        <div class="section_paciente bg-white">
+        <div class="section_paciente">
             <x-formPersona perfil="Paciente" :tipos_documento="$tipos_documento" :ciudades="$ciudades" :eps="$eps"/>
         </div>
         <div x-data="{ open: false }">
@@ -18,10 +18,7 @@
         </div>
         <form  method="post" id="crearOrden" action="{{ route('ordenes.store') }}" class="mt-4">
             @csrf
-@dump($errors)
-
-
-    <h2 class="font-bold mb-4 text-xl text-secondary">Datos de la Orden</h2>
+    <h2 class="font-bold mb-4 text-xl text-text">Datos de la Orden</h2>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div class="w-full">
                 <x-input-label for="numero_orden">Número de Orden</x-input-label>
@@ -37,12 +34,12 @@
                 </div>
             </div>
         </div>
-        <section class="section_examenes bg-white p-4" >
-            <h2 class="font-bold mb-4 text-xl text-secondary">Exámenes</h2>
+        <section class="section_examenes p-4" x-data="{ examenes: [] }">
+            <h2 class="font-bold mb-4 text-xl text-text">Exámenes</h2>
 
                 @foreach($examenes as $examen)
-                    <div class="examen-item grid grid-cols-4 items-center gap-4 p-2 border-b border-gray-200">
-                        <input type="number" name="examenes[{{ $examen->id }}]" class="form-input w-16"  placeholder="0" min="0">
+                    <div class="examen-item grid grid-cols-4 items-center gap-4 p-2 border-b border-border">
+                        <input type="checkbox" name="examenes[{{ $examen->id }}]" @change="examenes.push({{ $examen->id }})" class="form-checkbox">
                         <span>{{ $examen->cup }}</span>
                         <span>{{ $examen->nombre }}</span>
                         <span>${{ $examen->valor }}</span>
@@ -52,7 +49,7 @@
                <input type="hidden" name="paciente_id" id="paciente_id" >
                <input type="hidden" name="acompaniante_id" id="acompaniante_id" >
 
-            <div class="w-full bg-white p-4 border border-gray-300 rounded-sm shadow-md my-4" x-data="{ open: false }">
+            <div class="w-full p-4 border border-borders rounded-sm shadow-md my-4" x-data="{ open: false }">
             <label for="mostrarObservaciones">Observaciones</label>
             <input type="checkbox" id="mostrarObservaciones" @change="open = $event.target.checked">
             <div x-show="open" x-transition>
