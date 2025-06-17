c0116eaad38cc2c28110ea465b3b4ed42c904bcb
Foto Aleph <fotoaleph@gmail.com>
2025-06-16 20:34:06 -0500

    guardar 1.0

--- app/Services/EscogerReferencia.php
+++ app/Services/EscogerReferencia.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Log;
 use App\Models\Persona;
 use App\Models\Parametro;
 use App\Models\Procedimiento;
+use App\Models\Resultado;
 use App\Models\ValorReferencia;
 use Carbon\Carbon;
 
@@ -89,7 +90,7 @@ class EscogerReferencia
                 'nombre' => $parametro->nombre,
                 'grupo' => $parametro->grupo,
                 'tipo_dato' => $parametro->tipo_dato,
-                'defult'=>$parametro->default,
+                'default'=>$parametro->default,
                 'metodo' => $parametro->metodo,
                 'unidades' => $parametro->unidades,
                 'referencia' => $referencia?$referencia['salida']:null,
@@ -105,8 +106,19 @@ class EscogerReferencia
 
         $dDemograficos = self::datosDemograficos($pro->orden->paciente,Carbon::parse($pro->fecha) );
 
-        foreach ( $formData as $paramtero => $resultado ){
-            $parametro = null;
+        foreach ( $formData as $parametro => $resultado ){
+            $parametro = Parametro::find($parametro);
+            $ref = self::estableceReferencia($dDemograficos,$parametro);
+            $isNormal = true;
+            if($ref !== null){
+                $isNormal = $resultado<$ref['max'] && $resultado>$ref['min'];
+            }
+            Resultado::create([
+                'parametro_id'=>$parametro->id,
+                'resultado'=>$resultado,
+                'procedimiento_id'=>$pro->id,
+                'is_normal'=>$isNormal
+            ]);
 
         }
 
