de2bfb1514ee65326bf7c0f2d2610250813628b5
DrwnDrDev <drwn.dr.dev@gmail.com>
2025-06-20 13:08:32 -0500

    tamaños de impresion A5 A4 ajustados

--- resources/views/components/parametro-print.blade.php
+++ resources/views/components/parametro-print.blade.php
@@ -1,18 +1,18 @@
 @props(['item'])
 
 
-    <div class="Componente_param grid [grid-template-columns:2fr_1fr_1fr_1fr] gap-2 pl-2">
+    <div class="Componente_param grid [grid-template-columns:minmax(max-content,2fr)_1fr_1fr_minmax(max-content,1fr)] gap-2 pl-2">
          
             <div class="uppercase"><label>{{$item->parametro->nombre}}</label>
             <span class="block font-light text-[0.8rem] text-titles">{{$item->parametro->metodo}}</span>                       
             </div>
-            <div class="flex flex-row-reverse justify-between">
+            <div class="flex flex-row-reverse">
                 <p id="{{$item->id}}">{{$item->resultado}}</p>
             @if (!$item->es_normal)  
-                    <span class="font-bold pl-20">
+                    <span class="font-bold pr-4">
                     *
                     </span>
-                @endif    
+            @endif    
             
             </div>
             <div><p class="font-light">{{$item->parametro->unidades}}</p></div>
--- resources/views/resultados/show.blade.php
+++ resources/views/resultados/show.blade.php
@@ -3,27 +3,27 @@
         resultado_{{$procedimiento->orden->paciente->tipo_documento}}
     </x-slot>
 
-    <article class="print_resultado max-w-6xl mx-auto sm:p-2 md:p-4 lg:p-6 print:text-[0.6rem] print:!p:0">
+    <article class="print_resultado relative max-w-6xl mx-auto sm:p-2 md:p-4 lg:p-6 print:!text-xs print:!p-0 print:!bg-white print:!text-black">
         <section class="print_header hidden print:flex w-full">
             <figure class="w-12 mx-auto p-0">
-            <x-application-logo/>
+                <x-application-logo />
             </figure>
             <div>
-            <h1 class="font-bold"></h1>
-            <h2 class="font-semibold">Hospital San Juan de Dios</h2>
-            <p class="">Calle 123 # 456 - 789</p>
-            <p class="">Teléfono: (123) 456-7890</p>
-            <p class="">Email: </p>
+                <h1 class="font-bold"></h1>
+                <h2 class="font-semibold">Hospital San Juan de Dios</h2>
+                <p class="">Calle 123 # 456 - 789</p>
+                <p class="">Teléfono: (123) 456-7890</p>
+                <p class="">Email: </p>
             </div>
         </section>
-        
+
         <section class="print_paciente grid grid-cols-2 py-4 border-t border-b border-borders w-full">
             <div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-0">
 
                 <span class="font-bold ">Paciente: </span>
                 <h3>{{$procedimiento->orden->paciente->nombreCompleto()}}</h3>
                 <span class="font-bold ">Identificación: </span>
-                
+
                 <div class="flex flex-wrap">
                     <h3>{{$procedimiento->orden->paciente->tipo_documento}}{{$procedimiento->orden->paciente->numero_documento}}</h3>
                     <div class="flex pl-4 gap-2">
@@ -45,10 +45,10 @@
 
             </div>
         </section>
-            
-        <section class="print_paramentros w-full print:!text-[0.5rem]">
+
+        <section class="print_paramentros w-full">
             <h1 class="font-bold text-center my-4 uppercase">{{$procedimiento->examen->nombre}}</h1>
-            <div class="header_resultado grid [grid-template-columns:2fr_1fr_1fr_1fr] gap-2 pl-4 mb-2 bg-secondary text-titles">
+            <div class="header_resultado grid [grid-template-columns:minmax(max-content,2fr)_1fr_1fr_minmax(max-content,1fr)] gap-2 pl-4 mb-2 bg-secondary text-titles">
                 <h2 class="font-semibold">Parametro</h2>
                 <h2 class="font-semibold text-end">Resultado</h2>
                 <h2 class="font-semibold">U. Medida</h2>
@@ -70,8 +70,19 @@
 
             @endforeach
             @dump($procedimiento->resultado)
-        </section>    
-</article>
+        </section>
+        <footer class="print_footer sticky bottom-0 left-0 w-full print:!text-[0.6rem] items-center mt-4">
+            <div class="">
+                <p class="font-semibold">Fecha de impresión: {{ now()->format('d/m/Y H:i') }}</p>
+                <p class="font-semibold">Usuario: {{ auth()->user()->name }}</p>
+            </div>
+            <div class="">
+                <p class="font-semibold">Firma del médico:</p>
+                <p class="border-b border-borders w-48 h-8"></p>
+            </div>
+
+        </footer>
+    </article>
 
 
 
