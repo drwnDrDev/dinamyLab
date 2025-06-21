84d2968c330625abd9f9faee78bf809e0273ff4f
Foto Aleph <fotoaleph@gmail.com>
2025-06-20 20:44:33 -0500

    facturas tabla

--- database/migrations/2025_05_15_005900_create_impuestos_table.php
+++ database/migrations/2025_05_15_005900_create_impuestos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('impuestos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre')->unique();
+            $table->string('impuesto');
             $table->string('descripcion')->nullable();
             $table->string('codigo')->unique();
             $table->decimal('tasa', 5, 2);
