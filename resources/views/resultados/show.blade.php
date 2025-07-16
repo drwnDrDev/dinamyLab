99a3301f78420b2df8b7a4b4c559913ff8a8fd5f
DrwnDrDev <drwn.dr.dev@gmail.com>
2025-07-16 16:44:06 -0500

    firma y fuentes corregidas

--- resources/views/resultados/show.blade.php
+++ resources/views/resultados/show.blade.php
@@ -4,58 +4,58 @@
     </x-slot>
 
     <article class="print_resultado relative max-w-6xl mx-auto sm:p-2 md:p-4 lg:p-6 print:!min-h-screen print:!text-xs print:!p-0 print:!bg-white print:!text-black">
-        <section class="print_header p-4 flex w-full">
-            <figure class="w-12 my-auto p-0">
+        <section class="print_header pb-2 flex items-center w-full">
+            <figure class="w-16 my-auto p-0">
 
-                           @if (session('sede')->id === $procedimiento->sede->id)
-
-                                 <img class="h-10 w-auto fill-current text-gray-800" src="{{ asset('storage/logos/'.$procedimiento->sede->logo) }}" alt="{{ session('sede')->nombre }}">
-
-
-                           @else
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                           @endif()
+                @if (session('sede')->id === $procedimiento->sede->id)
+                <img class="fill-current object-contain text-gray-800" src="{{ asset('storage/logos/'.$procedimiento->sede->logo) }}" alt="{{ session('sede')->nombre }}">
+                @else
+                <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                @endif()
             </figure>
             <div class="pl-4">
-
-                <h2 class="font-semibold"></h2>
-                <p class="font-light">{{$procedimiento->sede->nombre}}</p>
-                <p class="font-semibold">{{$procedimiento->sede->empresa->nombre_comercial}}</p>
+                <h2 class="font-semibold text-xl">{{$procedimiento->sede->empresa->nombre_comercial}}</h2>
+                <p class="">{{$procedimiento->sede->nombre}}</p>
+                <p class="font-light">NIT. {{$procedimiento->sede->empresa->nit}}</p>
             </div>
         </section>
 
         <section class="print_paciente grid grid-cols-2 py-2 border-t-[0.2px] border-b-[0.2px] border-borders w-full">
+            <div class="col-span-2 flex gap-2">
+                <span class="font-normal ">Paciente: </span>
+                <h3 class="text-titles font-light">{{$procedimiento->orden->paciente->nombreCompleto()}}</h3>
+            </div>
             <div class="w-full grid grid-cols-[auto_1fr] gap-x-4 gap-y-0">
-
-                <span class="font-bold ">Paciente: </span>
-                <h3 class="text-md">{{$procedimiento->orden->paciente->nombreCompleto()}}</h3>
-                <span class="font-bold ">Identificación: </span>
-                <h3>{{$procedimiento->orden->paciente->tipo_documento->cod_rips}}{{$procedimiento->orden->paciente->numero_documento}}</h3>
+                
+                <span class="font-normal ">Identificación: </span>
+                <h3 class="text-titles font-light">{{$procedimiento->orden->paciente->tipo_documento->cod_rips}}{{$procedimiento->orden->paciente->numero_documento}}</h3>
 
 
-                    <div class="flex gap-2">
-                        <span class="font-bold ">Sexo: </span>
-                        <h3>{{$procedimiento->orden->paciente->sexo}} </h3>
-                    </div>
-                    <div class="flex gap-2">
-                        <span class="font-bold ">Edad: </span>
-                        <h3>{{$procedimiento->orden->paciente->edad()}}</h3>
-                    </div>
+                <div class="flex gap-2">
+                    <span class="font-normal ">Sexo: </span>
+                    <h3 class="text-titles font-light">{{$procedimiento->orden->paciente->sexo==="M" ? 'Masculino':'Femenino'}} </h3>
+                </div>
+                <div class="flex gap-2">
+                    <span class="font-normal ">Edad: </span>
+                    <h3 class="text-titles font-light">{{$procedimiento->orden->paciente->edad()}}</h3>
+                </div>
 
 
             </div>
             <div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-0">
-                <span class="font-bold ">Fecha de atención: </span>
-                <h3>{{$procedimiento->orden->created_at->format('d-m-Y')}}</h3>
-               <h3><span class="font-bold ">Órden Nº: </span>
-                {{$procedimiento->orden->numero}}</h3>
+                <span class="font-normal ">Fecha de atención: </span>
+                <h3 class="text-titles font-light">{{$procedimiento->orden->created_at->format('d-m-Y')}}</h3>
+                <span>Fecha de Nacimiento: </span>
+                <h3 class="text-titles font-light">{{$procedimiento->orden->paciente->fecha_nacimiento->format('d-m-Y')}}</h3>
+                <span class="font-normal ">Órden Nº: </span>
+                <h3 class="text-titles font-light">{{$procedimiento->orden->numero}}</h3>
 
             </div>
         </section>
 
         <section class="print_paramentros w-full">
-            <h1 class="font-bold text-center my-4 uppercase">{{$procedimiento->examen->nombre}}</h1>
-            <div class="header_resultado grid [grid-template-columns:minmax(25%,40%)_20%_20%_minmax(max-content,20%)] gap-2 pl-4 mb-2 bg-secondary text-titles">
+            <h1 class="font-semibold text-center my-4 uppercase">{{$procedimiento->examen->nombre}}</h1>
+            <div class="header_resultado grid [grid-template-columns:minmax(25%,40%)_20%_20%_minmax(max-content,20%)] gap-2 pl-4 mb-2 text-titles border-b border-t border-text">
                 <h2 class="font-semibold">Parametro</h2>
                 <h2 class="font-semibold text-end">Resultado</h2>
                 <h2 class="font-semibold">U. Medida</h2>
@@ -77,37 +77,40 @@
 
             @endforeach
 
-
-
         </section>
+        
+                <div class="text-end text-xs font-light p-4 print:hidden">
+                    <p class="text-xs font-light">Bacteriologo: {{$procedimiento->empleado->user->name}} - {{$procedimiento->empleado->numero_documento}}</p>
 
-            <aside class="firma  print:!block mt-4">
-
-             <img src=" {{ asset('storage/firmas/'.$procedimiento->empleado->firma) }}" alt="{{$procedimiento->empleado->user->name}}" class="w-32 h-16">
-
-
-            </aside>
-
+                </div>      
 
         <footer class="print_footer hidden bottom-0 left-0 w-full m-auto items-center print:!block print:!absolute">
+            <aside class="firma flex justify-end h-full items-end p-4 gap-2">
+
+                <div class="flex flex-col">
+                    <img src=" {{ asset('storage/firmas/'.$procedimiento->empleado->firma) }}" alt="{{$procedimiento->empleado->user->name}}" class="w-32 object-contain">
+                    <span class="text-xs font-light">Bacteriologo: {{$procedimiento->empleado->user->name}} - {{$procedimiento->empleado->numero_documento}}</span>
+
+                </div>
+            </aside>
             <div class="font-light border-t border-borders text-center p-2">
 
                 <spam class="">
-                  <span class="text-sm font-semibold">☎</span>
-                  @foreach ( $procedimiento->sede->telefonos as $telefono)
-                    <span class="text-sm"> {{$telefono->numero}}</span>
+                    <span class="text-xs">Tels: </span>
+                    @foreach ( $procedimiento->sede->telefonos as $telefono)
+                    <span class="text-xs"> {{$telefono->numero}}</span>
                     @if (!$loop->last)
-                    <span class="text-sm">- </span>
+                    <span class="text-xs">- </span>
                     @endif
 
-                  @endforeach
+                    @endforeach
                 </spam>
-                <spam class="">Dirección: {{$procedimiento->sede->direccion->direccion}}</spam>
+                <spam class="text-xs">Dirección: {{$procedimiento->sede->direccion->direccion}}</spam>
                 @if ($procedimiento->sede->emails && count($procedimiento->sede->emails) > 0)
-                    <spam class="">Email: {{$procedimiento->sede->emails->first()->email}} </spam>
+                <spam class="text-xs">Email: {{$procedimiento->sede->emails->first()->email}} </spam>
 
                 @elseif ($procedimiento->sede->empresa->emails && count($procedimiento->sede->empresa->emails) > 0)
-                    <spam class="">Email: {{$procedimiento->sede->empresa->emails->first()->email}} </spam>
+                <spam class="text-xs">Email: {{$procedimiento->sede->empresa->emails->first()->email}} </spam>
                 @endif
 
             </div>
@@ -117,4 +120,3 @@
 
 
 </x-app-layout>
-
