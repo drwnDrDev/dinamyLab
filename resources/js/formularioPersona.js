4b40c2a11e987b00120cf4dc250825c476fd7d10
Julian David Ramirez <jramirezman@unal.edu.co>
2025-07-23 10:16:06 -0500

    correccion municipios autocompletado

--- resources/js/formularioPersona.js
+++ resources/js/formularioPersona.js
@@ -12,10 +12,11 @@
         form['correo'].value= persona.correo;
         form["eps"].value=persona.eps;
         form["direccion"].value=persona.direccion;
+        displayMunicipios(form["municipio"]);
         form["municipio"].value=persona.municipio;
+        form["municipioBusqueda"].value=appState.municipios.find(m => m.codigo === persona.municipio)?.municipio || '';
         form["municipio"].classList.remove('hidden');
         form["municipioBusqueda"].classList.add('hidden');
-        form["municipioBusqueda"].value=appState.municipios.find(m => m.codigo === persona.municipio)?.municipio || '';
         form["pais"].value=persona.pais;
         form.querySelectorAll('input[name="sexo"]').forEach(radio => {
             radio.checked = radio.value === persona.sexo;
