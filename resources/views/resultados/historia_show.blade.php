1b63b8c8a12a121661f2b0c1a865bd4b830d9e86
DrwnDrDev <drwn.dr.dev@gmail.com>
2025-07-25 23:21:33 -0500

    select historia agregada

--- resources/views/resultados/historia_show.blade.php
+++ resources/views/resultados/historia_show.blade.php
@@ -1,4 +1,11 @@
 <x-app-layout>
-    <h1>Aqui va la VISTA de los procedimiento a imprimir</h1>
+    
+    <div class="py-4 flex justify-between items-center border-b border-borders">
+        <div>
+            <p class="text-2xl font-bold leading-tight tracking-[-0.015em]">{{ $persona->nombreCompleto() }}</p>
+            <p class="text-titles">Paciente ID: {{ $persona->numero_documento }}</p>
+        </div>
+    </div>
+    <h3>Aqui va la VISTA de los procedimiento a imprimir</h1>
 
 </x-app-layout>
