77914f2642c2c34460e33bdf29b9ddce82d4ebb9
Julian David Ramirez <jramirezman@unal.edu.co>
2025-09-02 08:36:29 -0500

    Fromten esctaic

--- app/Http/Controllers/Api/FrontendDataController.php
+++ app/Http/Controllers/Api/FrontendDataController.php
@@ -7,6 +7,13 @@ use App\Models\TipoDocumento;
 use App\Models\Pais;
 use App\Models\Municipio;
 use App\Models\Eps;
+use App\Models\ServicioHabilitado;
+use App\Models\ViaIngreso;
+use App\Models\ModalidadAtencion;
+use App\Models\TipoAfiliacion;
+use App\Models\CausaExterna;
+use App\Models\FinalidadConsulta;
+
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Cache; // Si usas caché con el Job
 
@@ -22,14 +29,29 @@ class FrontendDataController extends Controller
 
 
         try {
-            $cachedData = Cache::get('frontend_static_data');
-            if ($cachedData) {
-                return response()->json($cachedData);
-            }
+            // $cachedData = Cache::get('frontend_static_data');
+            // if ($cachedData) {
+            //     return response()->json($cachedData);
+            // }
+            $servicosHabilitados = ServicioHabilitado::where('activo', true)
+                                    ->orderBy('nivel','desc')->get();
+            $viaIngreso = ViaIngreso::where('activo', true)
+                                    ->orderBy('nivel','desc')->get();
 
-            $tiposDocumentoPaciente = TipoDocumento::where('es_paciente', true)->orderBy('nivel','desc')->get();
-            $tiposDocumentoPagador = TipoDocumento::where('es_pagador', true)->orderBy('nivel','desc')->get();
-            $paises = Pais::select('nombre', 'codigo_iso','nivel')->orderBy('nivel', 'desc')->get();
+            $tipoAfiliacion = TipoAfiliacion::where('activo', true)
+                                    ->orderBy('nivel','desc')->get();
+            $causaExterna = CausaExterna::where('activo', true)
+                                    ->orderBy('nivel','desc')->get();
+            $modalidadAtencion = ModalidadAtencion::where('activo', true)
+                                    ->orderBy('nivel','desc')->get();
+            $finaalidadConsulta = FinalidadConsulta::where('activo', true)
+                                    ->orderBy('nivel','desc')->get();
+            $tiposDocumentoPaciente = TipoDocumento::where('es_paciente', true)
+                                    ->orderBy('nivel','desc')->get();
+            $tiposDocumentoPagador = TipoDocumento::where('es_pagador', true)
+                                    ->orderBy('nivel','desc')->get();
+            $paises = Pais::select('nombre', 'codigo_iso','nivel')
+                                    ->orderBy('nivel', 'desc')->get();
             $municipios = Municipio::select('municipio', 'id','departamento')
                                     ->orderBy('nivel', 'desc')
                                     ->get()
@@ -57,6 +79,12 @@ class FrontendDataController extends Controller
                 'eps' => $eps,
                 'cups' => $cupsActivos,
                 'cie10' => $diacgonosticosActivos,
+                'servicios_habilitados' => $servicosHabilitados,
+                'via_ingreso' => $viaIngreso,
+                'modalidad_atencion' => $modalidadAtencion,
+                'tipo_afiliacion' => $tipoAfiliacion,
+                'causa_externa' => $causaExterna,
+                'finalidad_consulta' => $finaalidadConsulta,
             ];
 
         Cache::put('frontend_static_data', $data, now()->addHours(24)); // Cache por 24 horas
