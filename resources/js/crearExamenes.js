1ce6d6ece52e7d9f0bbb1cdb7334e722878fc158
Julian David Ramirez <jramirezman@unal.edu.co>
2025-09-06 09:52:54 -0500

    sancocho 1.0

--- resources/js/crearExamenes.js
+++ resources/js/crearExamenes.js
@@ -9,15 +9,31 @@ import {dom,appState} from './variables.js'
         const totalActual = (examen.currenTotal ?? (examen.valor * (examen.cantidad || 0))).toFixed(2);
 
         item.innerHTML = `
-            <div class="grid flex-grow">
+            <div class="grid grid-cols-2 flex-grow">
                 <label for="examen-${examen.id}" class="text-lg font-semibold">${examen.nombre}</label>
-                <span id="precio-${examen.id}" class="text-sm ${examen.currenTotal ? 'text-gray-900' : 'text-gray-500'} precio" >
+                <span id="precio-${examen.id}"
+                class="text-sm ${examen.currenTotal ? 'text-gray-900' : 'text-gray-500'} precio" >
                     $ ${totalActual}
                 </span>
+
+                <span class="${examen.currenTotal ? 'hidden' : 'flex w-24 h-10 text-sm'}" >
+                    CIE principal:
+                    <select name="cie_principal[${examen.id}]" class="w-32 ml-1 px-1 py-0.5 border border-borders rounded focus:outline-none focus:ring-2 focus:ring-primary">
+                        <option value="">Seleccione</option>
+                        <option value="A00">A00 - Cólera</option>
+                        <option value="B00">B00 - Herpesviral [herpes simple] infecciones</option>
+                        <option value="C00">C00 - Neoplasia maligna de labio</option>
+                        <option value="D00">D00 - Neoplasia in situ de cavidad bucal, esófago y estómago</option>
+                        <option value="E00">E00 - Trastornos por deficiencia de yodo</option>
+                        <option value="F00">F00 - Demencia en la enfermedad de Alzheimer</option>
+                    </select>
+                    </label>
+                </span>
             </div>
-            <input type="number"  id="examen-${examen.id}" name="examenes[${examen.id}]"
-                   class="w-20 px-2 py-1 text-center rounded border border-borders bg-white focus:outline-none focus:ring-2 focus:ring-primary"
+            <input type="number" id="examen-${examen.id}" name="examenes[${examen.id}]"
+                   class="flex w-20 px-2 py-1 text-center rounded border border-borders bg-white focus:outline-none focus:ring-2 focus:ring-primary"
                    min="0" value="${examen.cantidad || 0}" step="1">
+
         `;
         return item;
     };
--- resources/js/manejadorEventos.js
+++ resources/js/manejadorEventos.js
@@ -7,12 +7,9 @@ import { populateFormWithPersonaData, displayValidationErrors,displayMunicipios,
 
 export const handleFiltroExamenes = () => {
         const query = dom.busquedaExamenInput.value.toLowerCase();
-        const solo16k = dom.soloDiezYSeisMil.checked;
-
         appState.examenesVisibles = appState.todosLosExamenes.filter(examen => {
-            const pasaFiltroPrecio = !solo16k || parseFloat(examen.valor) === 16000;
             const pasaFiltroBusqueda = query.length < 1 || examen.nombre.toLowerCase().includes(query);
-            return pasaFiltroPrecio && pasaFiltroBusqueda;
+            return pasaFiltroBusqueda;
         });
 
         renderExamenes(appState.examenesVisibles);
@@ -70,7 +67,7 @@ export const handleBuscarMunicipio = (currentBusquedaFormMunicipio) => {
         }
 
         const persona = await guardarPersona(url,formData);
-        
+
         return persona;
     };
 
@@ -109,9 +106,16 @@ export const notificarGuardado = (persona,isPaciente=true,form) => {
                 examen.currenTotal = examen.valor * examen.cantidad;
 
                 const precioSpan = document.getElementById(`precio-${examen.id}`);
+                
+
+
                 if (precioSpan) {
                     precioSpan.textContent = `$ ${examen.currenTotal.toFixed(2)}`;
                     precioSpan.className = 'text-sm text-gray-900 dark:text-gray-100 precio';
+
+                    if (examen.currenTotal === 0) {
+                        precioSpan.classList.add('hidden');
+                    }
                 }
 
                 updateTotalExamenes();
