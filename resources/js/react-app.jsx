03006f8ec051a5187fdf7c1c0eda9de4a070d659
DrwnDrDev <drwn.dr.dev@gmail.com>
2025-09-18 15:17:11 -0500

    Refactor: Remove unnecessary React.StrictMode and clean up Vite config

--- resources/js/react-app.jsx
+++ resources/js/react-app.jsx
@@ -6,8 +6,8 @@ const container = document.getElementById('react-crear-orden');
 if (container) {
     const root = createRoot(container);
     root.render(
-        <React.StrictMode>
+        
             <CrearOrdenComponent />
-        </React.StrictMode>
+        
     );
 }
